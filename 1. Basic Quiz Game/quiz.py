c3d9f5ab7e814f48f09c97864da9f4622cb80bab
Aditya Mishra <mishraaaditya29@gmail.com>
2024-02-02 12:40:45 +0530

    project completed

--- 1. Basic Quiz Game/quiz.py
+++ 1. Basic Quiz Game/quiz.py
@@ -2,7 +2,45 @@ print("Welcome To Computer Quiz Game!!!")
 
 playing = input("Do you want to participate? ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
 print("Let's start the game!")
+marks = 0
+
+answer = input("What is time complexity of binary search? ")
+if answer.lower() == "ologn":
+    print('Correct!')
+    marks += 1
+else:
+    print("Incorrect!")
+    marks -= 0.33
+
+answer = input("The IA-32 system follows which of the following design?")
+if answer.lower() == "cisc":
+    print('Correct!')
+    marks += 1
+else:
+    print("Incorrect!")
+    marks -= 0.33
+
+answer = input(
+    "Which forms have a relation that contains information about a single entity?")
+if answer.lower() == "4nf":
+    print('Correct!')
+    marks += 1
+else:
+    print("Incorrect!")
+    marks -= 0.33
+
+answer = input(
+    "What is the best case runtime of linear search(recursive) algorithm on an ordered set of elements?")
+if answer.lower() == "o(1)":
+    print('Correct!')
+    marks += 1
+else:
+    print("Incorrect!")
+    marks -= 0.33
+
+print("You marked " + str(marks) + " questions correct!")
+print("Your score is: " + str((marks / 4) * 100) + "%.")
