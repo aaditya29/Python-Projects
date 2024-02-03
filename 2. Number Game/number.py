f71e1bf581b0be6fe318e12a399421d1d063e4b2
Aditya Mishra <mishraaaditya29@gmail.com>
2024-02-03 17:17:33 +0530

    git commit

--- 2. Number Game/number.py
+++ 2. Number Game/number.py
@@ -4,3 +4,11 @@ highest_range = input("Write a number of your chocie: ")
 
 if highest_range.isdigit():
     highest_range = int(highest_range)
+
+    if highest_range <= 0:
+        print("Please enter a positive number!")
+        quit()
+
+
+random_number = random.randint(0, highest_range)
+guesses = 0
