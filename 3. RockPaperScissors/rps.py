484be1c44826d3b466667d0863c10a6ac054f0d5
Aditya Mishra <mishraaaditya29@gmail.com>
2024-02-04 13:38:10 +0530

    git commit

--- 3. RockPaperScissors/rps.py
+++ 3. RockPaperScissors/rps.py
@@ -6,9 +6,18 @@ computer_wins = 0
 game = ["rock", "paper", "scissors"]
 
 while True:
-    user_input = input("Enter ROCK, PAPER, SCISSORS OR 0 To Quit: ").lower()
-    if user_input == "0":
+    user_choice = input("Enter ROCK, PAPER, SCISSORS OR 0 To Quit: ").lower()
+    if user_choice == "0":
         break
 
-    if user_input not in game:
+    if user_choice not in game:
         continue
+
+    option = random.randint(0, 1, 2)
+
+    # rock:0, paper:1, scissors:2
+    computer_choice = game[option]
+    print("Computer picked ", computer_choice + ".")
+
+    if user_choice == "rock" and computer_choice == "scissors":
+        print("YOU WON!")
