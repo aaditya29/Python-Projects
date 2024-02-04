180d879f77ea8530d320a1b92af8ac021a51d91f
Aditya Mishra <mishraaaditya29@gmail.com>
2024-02-04 12:52:17 +0530

    git commit

--- 3. RockPaperScissors/rps.py
+++ 3. RockPaperScissors/rps.py
@@ -6,4 +6,9 @@ computer_wins = 0
 game = ["rock", "paper", "scissors"]
 
 while True:
-    
+    user_input = input("Enter ROCK, PAPER, SCISSORS OR 0 To Quit: ").lower()
+    if user_input == "0":
+        break
+
+    if user_input not in game:
+        continue
