f8e7a4270e976c490ea69c17c94e8461a11812cf
Aditya Mishra <mishraaaditya29@gmail.com>
2024-02-05 13:17:44 +0530

    project completed

--- 5. Math Challenger/challenge.py
+++ 5. Math Challenger/challenge.py
@@ -22,3 +22,17 @@ input("Press enter to start!")
 print("---------------------")
 
 start_time = time.time()
+
+for i in range(TOTAL_PROBLEMS):
+    expr, answer = generate_problem()
+    while True:
+        guess = input("Problem #" + str(i + 1) + ": " + expr + " = ")
+        if guess == str(answer):
+            break
+        wrong += 1
+
+end_time = time.time()
+total_time = round(end_time - start_time, 2)
+
+print("----------------------")
+print("Nice work! Your total time was ", total_time, "seconds!")
