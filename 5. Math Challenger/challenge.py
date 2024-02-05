6a2a89d6d954009b39134012b89a86418159d05d
Aditya Mishra <mishraaaditya29@gmail.com>
2024-02-05 12:51:41 +0530

    git commit

--- 5. Math Challenger/challenge.py
+++ 5. Math Challenger/challenge.py
@@ -5,3 +5,20 @@ OPERATORS = ["+", "-", "*"]
 MIN_OPERAND = 3
 MAX_OPERAND = 12
 TOTAL_PROBLEMS = 15
+
+
+def generate_problem():
+    left = random.randint(MIN_OPERAND, MAX_OPERAND)
+    right = random.randint(MIN_OPERAND, MAX_OPERAND)
+    operator = random.choice(OPERATORS)
+
+    expr = str(left) + " " + operator + " " + str(right)
+    answer = eval(expr)
+    return expr, answer
+
+
+wrong = 0
+input("Press enter to start!")
+print("---------------------")
+
+start_time = time.time()
