af5b024d0fe346767f62b870759a67e14d2d5622
Aditya Mishra <mishraaaditya29@gmail.com>
2024-02-06 16:18:36 +0530

    git commit -m added racer numbers

--- 6. Turtle Racer/racer.py
+++ 6. Turtle Racer/racer.py
@@ -2,3 +2,14 @@
 
 def racer_numbers():
     racers = 0
+    while True:
+        racers = input('Type the number of racers (2 - 10): ')
+        if racers.isdigit():
+            racers = int(racers)
+        else:
+            print('Input is not a number. Please Try Again!!')
+            continue
+        if 2 <= racers <= 10:
+            return racers
+        else:
+            print('Number not in range 2-10. Try Again!')
