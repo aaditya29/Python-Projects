b881994d4c7644b967ba75b7adb5673e6fbe152b
Aditya Mishra <mishraaaditya29@gmail.com>
2024-02-10 19:17:07 +0530

    adding start screen

--- 7. Word Speed Tester/wst.py
+++ 7. Word Speed Tester/wst.py
@@ -2,15 +2,18 @@ import curses  # curses library in Python provides a terminal-independent way to
 from curses import wrapper
 
 
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr(0, 0, 'Welcome to typing speed test!')
+    stdscr.refresh()
+
+
 def main(stdscr):
     # stylising the texts on screen
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
-    stdscr.clear()
-    stdscr.addstr(0, 0, 'Welcome to typing speed test!')
-    stdscr.refresh()
     key = stdscr.getkey()  # waits for user to type on screen
     print(key)  # printing the key user typed
 
