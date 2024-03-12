6c41b7cc663e8a1e4cfa5a3174ed2e3d30a3e109
Aditya Mishra <mishraaaditya29@gmail.com>
2024-03-12 18:12:58 +0530

    git commit "get name from paths function added"

--- 8. Game Data Scripting/code.py
+++ 8. Game Data Scripting/code.py
@@ -38,6 +38,16 @@ def find_all_game_paths(source):  # to fetch game path
     return game_paths
 
 
+def get_name_from_paths(paths, to_strip):
+    new_names = []
+    for path in paths:
+        _, dir_name = os.path.split(path)
+        new_dir_name = dir_name.replace(to_strip, "")
+        new_names.append(new_dir_name)
+
+    return new_names
+
+
 def create_dir(path):
     if not os.path.exists(path):
         os.mkdir(path)
